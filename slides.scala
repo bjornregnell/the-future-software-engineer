4739c277174be3c384913f5bbf71e5d5b05409c3
bjornregnell <bjorn.regnell@cs.lth.se>
2024-08-19 16:03:38 +0200

    add slides stubb from taggy doc

---
+++ slides.scala
@@ -0,0 +1,17 @@
+//> using dep "taggy:taggy:0.0.5,url=https://github.com/bjornregnell/taggy/releases/download/v0.0.5/taggy_3-0.0.5.jar"
+//> using scala "3.5.0"
+
+//  run this command in terminal to create slides in target/out.pdf 
+//  scala-cli run .
+
+import taggy.*
+
+@main def run = slides.toPdf()
+
+def slides = document("Taggy Slide Example"):
+  frame("Greetings in two languages"):
+    p("Nice greetings:")
+    itemize:
+      p("English: Hello world!")
+      p("Swedish: Hej världen!")
+    p("https://github.com/bjornregnell/taggy")
