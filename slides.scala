c30a782a84a905ab4456dbb6071d74dfbbc522b3
bjornregnell <bjorn.regnell@cs.lth.se>
2024-08-25 16:04:13 +0200

    update slides

--- slides.scala
+++ slides.scala
@@ -9,6 +9,7 @@ import taggy.*
 def pause = Latex.cmd{"pause"}
 def and = "\\&"
 def nl = "~\\\\{}"
+def arrows = "$\\Longleftrightarrow$"
 
 @main def run = slides.toPdf()
 
@@ -38,26 +39,40 @@ def slides = document("The Future Software Engineer", author = "Björn Regnell")
     image(file = "../img/fortran.png", width = 1.0)
     p("Western Computer Proceedings (1957)")
 
+  frame("Software Engineers push the limits with abstraction"):
+    p(s"An eternal spiral: $nl **abstraction** $arrows **complexity**")
+    itemize:
+      p("More powerful abstractions allow us to build more complex systems, which require us to invent more powerful abstraction, which allow us to build more complex systems .... etc.")
+
+  frame("Developer productivity"):
+    image(file = "../img/productivity.png", width = 1.05)
+    p(s"$pause **productivity** $arrows **work environment** $arrows **code quality**")
+
+
   frame("Machine Lerning (ML) deficiencies"):
     p("Everyone seem to throw ML at any problem currently, but...")
+    textSize(10,12)
     itemize:
       p("The training process relies on randomness that gives emergent, unpredictable behavior")
-      p("Models are hallucinating at random occasions")
-      p("Model behavior is difficult to explain")
+      itemize:
+        p("Models are hallucinating at random occasions")
+        p("Model behavior is difficult to explain in detail")
       p("Training needs massive amount of data")
-      p("Bias in the data gives biased models")
+      p("Bias in training data gives biased models")
+      p("Over-training hamper a generalization ability")
       p("The training process consumes massive amount of energy")
       p("ML often consume more compute than symbolic AI, which can do a better job than ML in many applications")
+      p("ML is trained on the past not the future -- we still need human innovation")
       p("In safety-critical applications ML models need to be contained to protect humans from deadly behavior")
       p("ML drive monopolization by big tech even further")
 
   frame("My predictions"):
     itemize:
       p(s"AI will **NOT steal the jobs** of (junior) software engineers")
-      p(s"AI will help us **significantly raise the level of abstraction**")
-      p(s"$pause AI will make us **more productive** and help us **create even more complex systems**")
-      p(s"$pause AI will create **lots of bugs** (that will keep *senior* devs busy)")
-      p(s"$pause AI will enable us to finally $nl **stop nitty-gritty, low-level, error-prone boilerplate** $nl and start concentrate on the $nl **fun \\$and difficult parts of software engineering**")
+      p(s"$pause AI can help us **significantly raise the level of abstraction**")
+      p(s"$pause AI can make us **more productive** and help us **create even more complex systems**")
+      p(s"$pause AI *will* create **lots of bugs** (that will keep *senior* devs busy)")
+      p(s"$pause AI might enable us to finally $nl **stop nitty-gritty, low-level, error-prone boilerplate** $nl and start concentrate on the $nl **fun \\$and difficult parts of software engineering**")
       itemize:
         p(s"$pause domain knowledge, understanding users")
         p(s"$pause good requirements prioritization")
@@ -66,8 +81,8 @@ def slides = document("The Future Software Engineer", author = "Björn Regnell")
         p(s"$pause modern safe programming languages with new powerful abstraction mechanisms")
       p(s"$pause **Symbolic AI** will be back to help contain ML deficiencies")
 
-  frame("Soner than you think: Symbolic AI will be back!"):
-    p("Symbolic AI is made with ''normal'', explainable, reliable code that do human-like smart things with smart algoritms.")
+  frame(s"Soner than you think: $nl **Symbolic** AI will be back!"):
+    p("Symbolic AI: made with ''normal'', explainable, reliable code that does human-like, smart things with understandable algoritms.")
     itemize:
       p(s"constraint programming, logic programming, search algorithms, automatic proofs, SAT solvers, expert systems, automated planing and scheduling, ...")
       p("https://en.wikipedia.org/wiki/Symbolic_artificial_intelligence")
@@ -81,11 +96,6 @@ def slides = document("The Future Software Engineer", author = "Björn Regnell")
     textSize(8,10)
     p("\\hfill Wagner, Borg, Runeson, IEEE Software (January 2024)")
 
-  frame("Developer productivity"):
-    image(file = "../img/productivity.png", width = 1.0)
-    p("IEEE Transaction on Software Engineering (2021)")
-
-
   frame("My advice to **stay relevant** and get the **best jobs**"):
     textSize(10,12)
     itemize:
@@ -93,14 +103,18 @@ def slides = document("The Future Software Engineer", author = "Björn Regnell")
       p(s"$pause Learn how ML works **under the hood** (a.k.a linear algebra) ")
       itemize:
         p("Try this simple neural network to see principles and problems: https://github.com/bjornregnell/scai")
-      p(s"Dig into **Symbolic AI** (will resurrect (?) during next AI winter)")
-      p(s"Understand coming **AI regulation** and its implications: $nl new features -> more code -> more bugs -> more jobs")
+      p(s"Dig into **Symbolic AI** (will resurrect during next AI winter (?))")
+      p(s"Understand coming **AI regulation** and its implications: $nl $pause new features -> more code -> more bugs -> more jobs")
       p(s"$pause Learn **safe** and **powerful** programming languages $nl $pause such as Scala https://www.scala-lang.org/ to boost your productivity and help you create high-quality, bug-free code")
-      p(s"$pause Take responsible for understanding the **''why''** and get the **big picture** and **deep domain knowledge** and develop your **decision-making skills** $nl $pause a.k.a Requirements Engineering")
+      p(s"$pause Take responsible for understanding the **''why''** and get the **big picture** and **deep domain knowledge** and develop your **decision-making skills** and **quality trade-offs** $nl $pause a.k.a Requirements Engineering")
       p(s"$pause Choose an employer that lets you **learn on the job**, gives you **challenges with mandate** and is **ethical**")
 
   frame("THANK YOU!"):
-    p("Links:")
+    textSize(10,10)
     itemize:
-      p("These slides: TODO")
-      p(s"Navigating the EU AI Act (Wagner, Borg, Runeson) $nl https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=10372509")
+      p(s"These slides: TODO")
+      p(s"Paper on developers' productivity by researchers at Google: https://web.eecs.umich.edu/~movaghar/What_Predicts_Software_Developers_Productivity%20IEEE-TSE%202019.pdf")
+      p(s"Sofware Quality with the QUPER model (video from Öredev): https://cs.lth.se/krav/quality-requirements/")
+      p(s"Papers by Björn Regnell: $nl https://scholar.google.com/citations?user=W37CtAwAAAAJ")
+      p(s"Navigating the EU AI Act (Wagner, Borg, Runeson): $nl https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=10372509")
+      p(s"Top paid devs use Scala: $nl https://survey.stackoverflow.co/2024/work#3-salary-and-experience-by-language")
